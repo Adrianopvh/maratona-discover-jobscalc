ce39e34dcec85999ecf2335e13079379e6efe12e
Adrianopvh <adriano.rodriques12389@gmail.com>
2021-04-13 23:06:12 -0400

    Implementando EJS no projeto

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,81 @@ const routes = express.Router();
 
 const views = __dirname + '/views/'
 
-routes.get('/', (req, res) => res.render(views + "index"));
+const profile = {
+    name: "Adriano Rodrigues",
+    avatar: "https://github.com/Adrianopvh.png",
+    "monthly-budget": 7000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4,
+    "value-hour": 75
+}
+
+const jobs = [
+    {
+    id: 1,
+    name: "Pizzaria Guloso",
+    "daily-hours": 4,
+    "total-hours": 120,
+     created_at: Date.now(),
+    },
+    {
+    id: 2,
+    name: "OneTwo Project",
+    "daily-hours": 6,
+    "total-hours": 94,
+     created_at: Date.now(),
+    }
+];
+
+function remainingDays(job) {
+    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+        
+        const createdDate = new Date(job.created_at)
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+        const dueDateInMs = createdDate.setDate(dueDay)
+
+        const timeDiffInMs = dueDateInMs - Date.now()
+
+        const dayInMs = 1000 * 60 * 60 * 24
+        const dayDIff = Math.floor(timeDiffInMs / dayInMs)
+
+        return dayDIff
+}
+
+routes.get('/', (req, res) => {
+
+    const updatedJobs = jobs.map((job) => {
+        const remaining = remainingDays(job)
+        const status = remaining <= 0 ? 'done' : 'progress'
+
+        return {
+            ...job,
+            remaining,
+            status,
+            budget: profile["value-hour"] * job["total-hours"]
+        }
+    });
+    
+return res.render(views + "index", { jobs: updatedJobs })
+
+});
+
 routes.get('/job', (req, res) => res.render(views +"job"));
+routes.post('/job', (req, res) => {
+
+    const lastId = jobs[jobs.length - 1]?.id || 1;
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now()
+    });
+    return res.redirect('/')
+});
 routes.get('/job/edit', (req, res) => res.render(views +"job-edit"));
-routes.get('/profile', (req, res) => res.render(views +"profile"));
+routes.get('/profile', (req, res) => res.render(views +"profile", { profile }));
 
 module.exports = routes;
